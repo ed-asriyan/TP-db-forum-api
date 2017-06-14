9f2ed821b2b915e33dab2b1458010c6358257b4a
Eduard Asriyan <asriyan1998@yandex.ru>
2017-06-14 13:53:23 +0300

    Added indexes.

---
+++ src/main/resources/db/migration/V2__index.sql
@@ -0,0 +1,31 @@
+DROP INDEX IF EXISTS forums_user_idx;
+DROP INDEX IF EXISTS threads_author_idx;
+DROP INDEX IF EXISTS threads_forum_idx;
+DROP INDEX IF EXISTS posts_author_idx;
+DROP INDEX IF EXISTS posts_forum_idx;
+DROP INDEX IF EXISTS votes_thread_idx;
+DROP INDEX IF EXISTS forum_users_user_id_idx;
+DROP INDEX IF EXISTS forum_users_forum_idx;
+
+--
+
+CREATE INDEX ON forums ("user");
+
+--
+
+CREATE INDEX ON threads (author);
+CREATE INDEX ON threads (forum);
+
+--
+
+CREATE INDEX ON posts (author);
+CREATE INDEX ON posts (forum);
+
+--
+
+CREATE INDEX ON votes (thread);
+
+--
+
+CREATE INDEX ON forum_users (user_id);
+CREATE INDEX ON forum_users (forum);
