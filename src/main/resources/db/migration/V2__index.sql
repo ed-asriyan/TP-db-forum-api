4c4c161672d9e4e8526a4f741e3eb6b491190905
Eduard Asriyan <asriyan1998@yandex.ru>
2017-06-14 14:26:14 +0300

    Added indexes for sortings

--- src/main/resources/db/migration/V2__index.sql
+++ src/main/resources/db/migration/V2__index.sql
@@ -3,6 +3,8 @@ DROP INDEX IF EXISTS threads_author_idx;
 DROP INDEX IF EXISTS threads_forum_idx;
 DROP INDEX IF EXISTS posts_author_idx;
 DROP INDEX IF EXISTS posts_forum_idx;
+DROP INDEX IF EXISTS posts_thread_path_idx;
+DROP INDEX IF EXISTS posts_root_id_path_idx;
 DROP INDEX IF EXISTS votes_thread_idx;
 DROP INDEX IF EXISTS forum_users_user_id_idx;
 DROP INDEX IF EXISTS forum_users_forum_idx;
@@ -15,6 +17,8 @@ CREATE INDEX ON forums ("user");
 
 CREATE INDEX ON threads (author);
 CREATE INDEX ON threads (forum);
+CREATE INDEX ON posts (thread, path);
+CREATE INDEX ON posts (root_id, path);
 
 --
 
